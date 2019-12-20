022221284f2b9070fe6b7ef97909bd0c353d56a6
Michael Wali <walimike139@gmail.com>
2019-12-21 00:36:01 +0300

    add pagination for app

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,7 +4,7 @@ class ArticlesController < ApplicationController
   # GET /articles
   # GET /articles.json
   def index
-    @articles = Article.all
+    @articles = Article.paginate(page: params[:page], per_page: 6).order('id DESC')
   end
 
   # GET /articles/1
